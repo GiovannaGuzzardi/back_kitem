8dbc86f032be0dbb7ce04166e3f74a41b30d7272
Giovanna Guzzardi <giovannaguzz13@gmail.com>
2025-08-11 20:34:22 -0300

    "deploy: adicona allowed_hosts"

--- kiItem/settings.py
+++ kiItem/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-rn5oqb*lylx$xxyhd973dcdp2+@il(@avs+8ndga9&9^39887b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['back-kitem-e12u.onrender.com']
 
 
 # Application definition
